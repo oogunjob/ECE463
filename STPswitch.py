610c6e9d072a4157a43bdd4b714dc0e5955076df
Tosin Ogunjobi <oogunjob@purdue.com>
2021-10-03 10:43:28 -0400

    changes from last night

--- STPswitch.py
+++ STPswitch.py
@@ -16,13 +16,25 @@ class STPswitch(Switch):
     def __init__(self, addr, heartbeatTime):
         Switch.__init__(self, addr, heartbeatTime)  # initialize superclass - don't remove
         """TODO: add your own class fields and initialization code here"""
-        cost = 1
-
+        self.root = 0
+        self.cost = 0
+        self.original = 0
+        self.hop = 0
 
     def handlePacket(self, port, packet):
         """TODO: process incoming packet"""
         # default implementation sends packet back out the port it arrived
-        self.send(port, packet)
+        print(self.addr, packet.content)
+
+        self.root = self.addr
+        self.original = self.addr
+        self.hop = self.addr
+
+        packet_temp = [str(self.root), str(self.cost), str(self.original), str(self.hop)]
+        control = Packet(2, packet.srcAddr,packet.dstAddr,",".join(packet_temp))
+
+
+        self.send(port, control)
 
 
     def handleNewLink(self, port, endpoint, cost):
