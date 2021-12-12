799b00b532749f5fc96a64fa179e84aef740a9ee
Tosin Ogunjobi <oogunjob@purdue.com>
2021-12-12 16:52:59 -0500

    commit on Tosin

--- PA04/http_client/http_client.c
+++ PA04/http_client/http_client.c
@@ -16,14 +16,115 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 
-#define MAXDATA 1024
+#define MAXBUFFER 4096
 
 char* getFileName(char* filepath);
-int HTTPStatus(int sock);
-int computeFileSize(int sock);
+int CheckStatus(int sock);
+int CheckContentLength(int sock);
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4) {
+        fprintf(stderr,"usage: ./http_client [host] [port number] [filepath]\n");
+        exit(1);
+    }
+
+    // stores command line arguments in variables
+    char* domain = argv[1];
+    int PORT = (int) strtol(argv[2], NULL, 10);
+    char* filepath = argv[3];
+    char* filename = getFileName(filepath);
+
+    // variables for sock construction
+    int sock;
+    struct sockaddr_in server_addr;
+    struct hostent *host;
+
+    int bytesReceived;
+    char send_data[MAXBUFFER];
+    char recv_data[MAXBUFFER];
+    char clientMessage[MAXBUFFER];
+    int contentLength;
+
+    // connects to the host
+	if ((host = gethostbyname(domain)) == NULL) {
+		herror("gethostbyname");
+		exit(1);
+	}
+
+    // creation of TCP socket
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket");
+		exit(1);
+	}
+
+    // creates assignment of socket attributes
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT);
+	server_addr.sin_addr = *((struct in_addr *)host->h_addr_list[0]);
+	bzero(&(server_addr.sin_zero), 8);
+	
+    // connection to the socket
+    if (connect(sock, (struct sockaddr *) &server_addr, sizeof(struct sockaddr)) < 0) {
+		perror("connect");
+		exit(1);
+	}
+
+    // GET request that is sent to the server
+    snprintf(send_data, sizeof(send_data), "GET /%s HTTP/1.1\r\nHost: %s:%s\r\n\r\n", filepath, domain, argv[2]);
+
+    // sends the request to the server
+    if(send(sock, send_data, strlen(send_data), 0) < 0){
+        perror("send");
+        exit(1); 
+    }
+
+    // first thing that needs to to be done is check the http status
+    // IF THE RESPONSE IS NOT 200, PRINT IT AND EXIT THE PROGRAM'
+    if(CheckStatus(sock) == 200){
+        
+        // if the response is 200 OK, check if the content size is correct and save the content to a filepath
+        if(contentLength = CheckContentLength(sock)){
+            // save to the file path
+            
+            int bytes = 0; // current number of bytes written to file
+            FILE* file = fopen(filename, "w"); // opens the file to be written to
+
+            while(bytesReceived = recv(sock, recv_data, MAXBUFFER, 0)){
+                if(bytesReceived == -1){
+                    perror("recieve");
+                    exit(1);
+                }
+
+                fwrite(recv_data, 1, bytesReceived, file); // writes bytes to file
+                bytes += bytesReceived;
+
+                if(bytes == contentLength){
+                    break;
+                }
+            }
+            fclose(file); // closes the file on completion
+        }
+
+        // if the resonse did not contain the content length, print an error message
+        else{
+            fprintf(stdout, "Error: could not download the requested file (file length unknown)\n");
+        }
+
+    // prints the first line of the response from the server
+    }
+    else{
+        fprintf(stdout, "The server responded with: \n");
+    }
+    
+    close(sock); // closes the socket
+    
+    return 0;
+}
 
 char* getFileName(char* filepath){
-    char *filename = filepath + strlen(filepath); // filename
+    char *filename; // filename
+    filename = filepath + strlen(filepath);
     
     // extracts filename from path
     for (; filename > filepath; filename--){
@@ -32,161 +133,75 @@ char* getFileName(char* filepath){
             break;
         }
     }
-
+    
+    fprintf(stdout, "%s\n", filename);
     return filename; // returns filename
 }
 
-int FindContentLength(int sock){
+int CheckContentLength(int sock){
+    char c;
     
-    char buffer[MAXDATA] = {0};
-    char* ptr = buffer + 4;
+    char buff[MAXBUFFER] = "";
+    char* ptr= buff + 4;
     
-    int bytesReceived;
-    
-    while(bytesReceived = recv(sock, ptr, 1, 0)){
-        if(bytesReceived < 0){
-            perror("FindContentLength");
-            exit(1);
-        }
-
-        if((ptr[-3] == '\r')  && (ptr[-2] == '\n' ) && (ptr[-1] == '\r')  && (*ptr == '\n')) 
-            break;
-        
-        ptr++;
-    }
-
-    *ptr = 0;
-    ptr = buffer + 4;
-
-    if(bytesReceived){
-        ptr = strstr(ptr,"Content-Length");
-        
-        if(ptr){
-            sscanf(ptr,"%*s %d",&bytesReceived);
-        }
-        else{
-            bytesReceived = 0;
-        }
-    }
-
-    return bytesReceived; // Content-Length
-}
-
-int HTTPStatus(int sock){
-
-    char buffer[MAXDATA] = {0};
-    char* ptr = buffer + 1;
-    
-    int bytesReceived;
+    int bytes_received;
     int status;
     
-    while(bytesReceived = recv(sock, ptr, 1, 0)){
-        if(bytesReceived < 0){
-            perror("HTTPStatus");
+    printf("Begin HEADER ..\n");
+    while(bytes_received = recv(sock, ptr, 1, 0)){
+        if(bytes_received==-1){
+            perror("Parse Header");
             exit(1);
         }
 
-        if((ptr[-1] == '\r') && (*ptr == '\n')) 
+        if((ptr[-3]=='\r')  && (ptr[-2]=='\n' ) && (ptr[-1]=='\r')  && (*ptr=='\n')) 
             break;
         
         ptr++;
     }
 
     *ptr = 0;
-    ptr = buffer + 1;
+    ptr = buff + 4;
+    //printf("%s",ptr);
+
+    if(bytes_received){
+        ptr = strstr(ptr,"Content-Length:");
+        if(ptr){
+            sscanf(ptr,"%*s %d",&bytes_received);
+
+        }else
+            bytes_received=-1; //unknown size
+
+       printf("Content-Length: %d\n",bytes_received);
+    }
+    printf("End HEADER ..\n");
+    
+    return bytes_received;
+}
+
+int CheckStatus(int sock){
+
+    char buff[MAXBUFFER] = "";
+    char* ptr = buff + 1;
+    int bytes_received, status;
+
+    while(bytes_received = recv(sock, ptr, 1, 0)){
+        if(bytes_received == -1){
+            perror("ReadHttpStatus");
+            exit(1);
+        }
+
+        if((ptr[-1] == '\r')  && (*ptr =='\n')) break;
+        ptr++;
+    }
+
+    *ptr=0;
+    ptr = buff + 1;
 
     sscanf(ptr,"%*s %d ", &status);
 
-    if(bytesReceived > 0){
-        return status; // returns HTTP status code
-    }
+    printf("%s\n", ptr); // prints entire messsage, NEED TO REMOVE
+    printf("status = %d\n",status); // prints what the status of the request was, NEED TO REMOVE
 
-    return 0;
-}
-
-int main(int argc, char *argv[]){
-    if (argc != 4){
-        fprintf(stderr,"usage: ./http_client [host] [port number] [filepath]\n");
-        exit(1);
-    }
-
-    // stores command line arguments in variables
-    char* domain = argv[1];
-    int port = (int) strtol(argv[2], NULL, 10);
-    char* filepath = argv[3];
-    char* filename = getFileName(filepath);
-    FILE* file = NULL;
-
-    // variables for sock construction
-    int sock;
-    struct sockaddr_in server_addr;
-    struct hostent *host;
-
-    int bytesReceived;
-    char send_data[MAXDATA];
-    char recv_data[MAXDATA];
-
-	if ((host = gethostbyname(domain)) == NULL) {
-		herror("gethostbyname");
-		exit(1);
-	}
-
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("socket");
-		exit(1);
-	}
-
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(port);
-	server_addr.sin_addr = *((struct in_addr *)host->h_addr_list[0]);
-	bzero(&(server_addr.sin_zero), 8);
-	
-    if (connect(sock, (struct sockaddr *) &server_addr, sizeof(struct sockaddr)) < 0) {
-		perror("connect");
-		exit(1);
-	}
-
-    // data to send to host
-    snprintf(send_data, sizeof(send_data), "GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n", filepath, domain);
-
-    if(send(sock, send_data, strlen(send_data), 0) < 0){
-        perror("send");
-        exit(1); 
-    }
-    
-    int contentLength; // size of the file being downloaded
-
-    if(HTTPStatus(sock) && (contentLength = FindContentLength(sock))){
-
-        int bytes = 0; // current number of bytes written to file
-
-        file = fopen(filename, "w"); // opens the file to be written to
-
-        while(bytesReceived = recv(sock, recv_data, MAXDATA, 0)){
-            if(bytesReceived < 0){
-                perror("recieve");
-                exit(1);
-            }
-
-            // writes bytes to file
-            fwrite(recv_data, 1, bytesReceived, file);
-            bytes += bytesReceived;
-
-            // stops receiving bytes when the number of current bytes reaches file size
-            if(bytes == contentLength){
-                break;
-            }
-        }
-
-        fclose(file); // closes the file
-    }
-    
-    else if(contentLength == 0){
-        fprintf(stdout, "Error: could not download the requested file (file length unknown)\n"); // new line character needed?
-        // exit(1); // ?
-    }
-
-    close(sock); // closes the socket
-
-    return 0;
+    return status; // returns the HTTP Status code
 }
