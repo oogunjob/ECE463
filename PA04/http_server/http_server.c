3b0c6119a1bd3450cc1123f7ea2f2f0c324672ce
Tosin Ogunjobi <oogunjob@purdue.com>
2021-12-11 22:35:21 -0500

    space

--- PA04/http_server/http_server.c
+++ PA04/http_server/http_server.c
@@ -227,7 +227,7 @@ void respond(int client_sock, struct sockaddr_in client, int database_sock, stru
               fprintf(stdout, "404 Not Found\n");
               send(client_sock, "HTTP/1.0 404 Not Found\r\n", 24, 0);
               ret = write(client_sock, "HTTP/1.0 404 Not Found\r\n\r\n<html><body><h1>404 Not Found</h1></body></html>", 74);
-            }
+            } 
 
             // if the picture was successfully found, return a 200 success message and send the response back to the server
             else{
